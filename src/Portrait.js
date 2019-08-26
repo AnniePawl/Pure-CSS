abbc9efaa98b8f7b9c110d68d7db2a0f1a54d80a
Anna Pawl <annampawl@gmail.com>
2019-08-26 16:49:38 -0700

    Title component

---
+++ src/Portrait.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+function Portrait() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+
+
+export default Portrait; 
---
+++ src/Title.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+function Title() {
+  return (
+    <div>
+      <h1 className="title">
+        CSS Art Collection
+      </h1>
+    </div>);
+}
+
+
+export default Title; 
