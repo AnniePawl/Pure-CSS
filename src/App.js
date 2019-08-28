72bcbb67be57c9ed010d13c71955269679014587
Anna Pawl <annampawl@gmail.com>
2019-08-27 18:26:23 -0700

    self-portrait in progress

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ArtDisplay from './ArtDisplay'
 import Title from './Title';
 import SmileyFace from './SmileyFace'
+import Portrait from './Portrait'
+
 import './App.css'
 
 
@@ -11,15 +13,18 @@ function App() {
       {/* Heading */}
       <Title title='Cascading Style Sheets Art Collection' />
 
-      {/* Smiley Face Art */}
-      <ArtDisplay title='Derpy Face' />
-      <SmileyFace />
+      <div className='display'>
+        {/* Smiley Face Art */}
+        <ArtDisplay title='Derpy Face' />
+        <SmileyFace />
 
 
-      {/* Portrait Art */}
-      <ArtDisplay title='Self Portrait' />
 
+        {/* Portrait Art */}
+        <ArtDisplay title='Self Portrait' />
+        <Portrait />
 
+      </div>
 
     </div>
   )
--- src/Portrait.js
+++ src/Portrait.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
+import './Portrait.css'
 
 function Portrait() {
   return (
-    <div>
-      <div>
-        <h2 className="title">Self Portrait</h2>
+    <div className='big_container'>
+
+      <div className='portrait_container'>
+        <div className='portrait_hair'></div>
+        <div className='oval'>
+
+          <div className='portrait_eyes'>
+
+            <div className='portrait_right_eye'>
+
+              <div className='portrait_brow'>
+
+              </div>
+              <div className='portrait_right_pupil'></div>
+              <div className='portrait_tiny_pupil'></div>
+
+            </div>
+            <div className='portrait_left_eye'>
+
+              <div className='portrait_brow'>
+
+              </div>
+              <div className='portrait_left_pupil'></div>
+              <div className='portrait_tiny_pupil'></div>
+            </div>
+          </div>
+          <div className='portrait_mouth'>
+            <div className="teeth">
+            </div>
+
+          </div>
+        </div>
+
+
+
       </div>
 
-      <div className="portrait_container"></div>
     </div>
   )
 }
