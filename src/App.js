77e196b232fd99a83a8347bf189fff8285acd7ae
Anna Pawl <annampawl@gmail.com>
2019-08-30 12:41:22 -0700

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import ArtDisplay from './ArtDisplay'
 import Title from './Title';
-import SmileyFace from './SmileyFace'
-import Portrait from './Portrait'
 import Lemon from './Lemon'
-import Olive from './Olive'
-import Pickle from './Pickle'
+
 
 
 import './App.css'
@@ -17,63 +13,10 @@ function App() {
       {/* Heading */}
       <Title title='Advanced Cascading Style Sheets ' subtitle="Art Collection" />
 
+      {/* Lemon Art */}
 
-      <div className='display'>
+      <Lemon />
 
-        <div
-          className='art_container'
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
-          }}
-        >
-          <ArtDisplay subtitle='Derpy Face' />
-          <div style={{
-            backgroundColor: 'red',
-            width: '130px',
-            height: '130px'
-          }}>
-
-          </div>
-        </div>
-
-        {/* Smiley Face Art */}
-        <div className='art_container'>
-          <ArtDisplay subtitle='Derpy Face' />
-          <SmileyFace />
-        </div>
-
-        {/* Lemon Art */}
-        <div className='art_container'>
-          <ArtDisplay subtitle='Lousy Lemon' />
-          <Lemon />
-        </div>
-
-
-        {/* Pickle Art */}
-        <div className='art_container'>
-          <ArtDisplay subtitle="I'm Kind of a Big Dill" />
-          <Pickle />
-        </div>
-
-        {/* Olive Art */}
-        <div className='art_container'>
-          <ArtDisplay subtitle='Olive You So Much It Hurts' />
-          <Olive />
-        </div>
-
-
-        {/* Tomato  Art */}
-
-        {/* Portrait Art */}
-        <div className='art_container'>
-          <ArtDisplay subtitle='Self Portrait' />
-          <Portrait />
-        </div>
-
-
-      </div>
 
     </div >
   )
