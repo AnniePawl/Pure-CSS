9d3433d852d37215bef959048e62909b0ec90558
Anna Pawl <annampawl@gmail.com>
2019-08-31 13:58:23 -0700

    blueberry in progress

---
+++ src/Blueberry.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Blueberry.css'
+
+function Blueberry() {
+  return (
+
+    <div className='blueberry_container'>
+
+
+      <div className='blueberry_leaves'>
+
+        <div className='blueberry_leaf_outer'>
+        </div>
+
+        <div className='blueberry_leaf_inner'> </div>
+
+      </div>
+
+
+
+
+      <div className='blueberry_base'>
+        <div className='blueberry_shadow'></div>
+
+      </div>
+
+    </div>
+
+
+
+  )
+}
+
+
+
+export default Blueberry
