3ecb8c713de739aaa9ccf83b12ba4b35aed1b8ce
Anna Pawl <annampawl@gmail.com>
2019-09-26 11:05:18 -0700

    Updates

---
+++ precache-manifest.90e089d6fc84a4336f7b0335246566ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32d221c647bc8e1d8b2da652d5fa0f0e",
+    "url": "/CSS-Art/index.html"
+  },
+  {
+    "revision": "5959bbfd522f21477a62",
+    "url": "/CSS-Art/static/css/main.20fb2ef3.chunk.css"
+  },
+  {
+    "revision": "27e7bd48768bf3e1d9e0",
+    "url": "/CSS-Art/static/js/2.7b3afc8f.chunk.js"
+  },
+  {
+    "revision": "5959bbfd522f21477a62",
+    "url": "/CSS-Art/static/js/main.e4143ccc.chunk.js"
+  },
+  {
+    "revision": "f8eeea596ecd24cf4c74",
+    "url": "/CSS-Art/static/js/runtime~main.d0ae41f0.js"
+  }
+]);
