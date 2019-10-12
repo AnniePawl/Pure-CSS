16e3ef66e7057149c669f7559af8d06415426594
Anna Pawl <annampawl@gmail.com>
2019-10-12 15:51:22 -0700

    orange in progress

---
+++ src/Orange/Orange.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Orange.scss'
+
+
+function Orange() {
+  return (
+
+    // Orange Container 
+    <div className='orange_container'>
+
+      <div className='orange_base'>
+        <div className='orange_shadow'>
+
+        </div>
+
+      </div>
+
+
+    </div>
+
+
+  )
+
+}
+
+
+export default Orange
