c0129a1d219bcbd6287cec0f4c3a6edaf725b7bf
Anna Pawl <annampawl@gmail.com>
2019-10-22 11:01:23 -0700

    adding google analytics

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ import { BrowserRouter, Route, Router, Link } from 'react-router-dom'
 import Display from './Display/Display'
 import FruitBag from './Fruits/FruitItems'
 import NavBar from './NavBar/Navbar'
+import ReactGA from 'react-ga'
 
-
+const trackingId = "UA-150726179-1"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+ReactGA.set({
+  userId: auth.currentUserId(),
+  // any data that is relevant to the user session
+  // that you would like to track with google analytics
+})
 
 function App() {
 
---
+++ src/Fruits/FruitHome.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FruitHome() {
+  return (
+
+  )
+}
+
+export default FruitHome
