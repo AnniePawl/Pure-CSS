fa822b072bfd7cb6693fba4422249c4261d48e2f
Anna Pawl <annampawl@gmail.com>
2019-10-22 11:11:05 -0700

    google analytics stuff

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ import ReactGA from 'react-ga'
 const trackingId = "UA-150726179-1"; // Replace with your Google Analytics tracking ID
 ReactGA.initialize(trackingId);
 ReactGA.set({
-  userId: auth.currentUserId(),
-  // any data that is relevant to the user session
-  // that you would like to track with google analytics
+  // userId: auth.currentUserId(),
+  // // any data that is relevant to the user session
+  // // that you would like to track with google analytics
 })
 
 function App() {
