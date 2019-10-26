7cb8d671e9d03fd6c5294a9baf4547e0e98c77de
Anna Pawl <annampawl@gmail.com>
2019-10-26 14:29:18 -0700

    lime and pom updates

--- src/Fruits/Lime/Lime.js
+++ src/Fruits/Lime/Lime.js
@@ -6,29 +6,32 @@ function Lime() {
 
     // lime Container
     <div className='lime_container'>
+      <div className='total'>
+        <div className='total_lime'>
 
-      <div className='total_lime'>
+          <div className='lime_shading'></div>
 
-        <div className='lime_shading'></div>
+          <div className='lime'>
 
-        <div className='lime'>
-
-          {/* lime Nobs/ Leaf */}
-          <div className='lime_nobs'>
-            <div className='lime_leaf'></div>
-            <div className='lime_top_nob'>
-              <div className='lime_top_nob_shading'></div>
-              <div className='lime_top_bud'></div>
+            {/* lime Nobs/ Leaf */}
+            <div className='lime_nobs'>
+              <div className='lime_leaf'></div>
+              <div className='lime_top_nob'>
+                <div className='lime_top_nob_shading'></div>
+                <div className='lime_top_bud'></div>
+              </div>
+              <div className='lime_bottom_nob'>
+                <div className='lime_bottom_nob_shading'></div>
+                <div className='lime_bottom_bud'></div>
+              </div>
             </div>
-            <div className='lime_bottom_nob'>
-              <div className='lime_bottom_nob_shading'></div>
-              <div className='lime_bottom_bud'></div>
-            </div>
+
           </div>
 
+          <div className='lime_shadow'></div>
         </div>
       </div>
-      <div className='lime_shadow'></div>
+
 
     </div>
 
--- src/Fruits/Pom/Pom.js
+++ src/Fruits/Pom/Pom.js
@@ -6,52 +6,54 @@ function Pom() {
   return (
     <div className='pom_container'>
 
+      <div className='total_pom'>
 
-      {/* Pom Top */}
-      <div className='pom_top'>
 
-        {/* Top Triangles */}
-        <div className='tops'>
-          <div className='pom_top1'></div>
-          <div className='pom_top2'></div>
-          <div className='pom_top3'></div>
+        {/* Pom Top */}
+        <div className='pom_top'>
+
+          {/* Top Triangles */}
+          <div className='tops'>
+            <div className='pom_top1'></div>
+            <div className='pom_top2'></div>
+            <div className='pom_top3'></div>
+          </div>
+          <div className='pom_top_base'></div>
+
         </div>
-        <div className='pom_top_base'></div>
 
-      </div>
-
-      <div className='pom_base'>
+        <div className='pom_base'>
 
 
 
-        <div className='pom_inner'>
+          <div className='pom_inner'>
 
-          {/* Pomegrante Seeds */}
-          <div className='all_pom_seeds'>
+            {/* Pomegrante Seeds */}
+            <div className='all_pom_seeds'>
 
-            {/* Seeds Set 1 */}
-            <div className='pom_seeds1 pom_seeds'>
-              <div className='pom_seed3 pom_seeds'></div>
-              <div className='pom_seed1 pom_seeds'></div>
-              <div className='pom_seed2 pom_seeds'></div>
-              <div className='pom_seed3 pom_seeds'></div>
+              {/* Seeds Set 1 */}
+              <div className='pom_seeds1 pom_seeds'>
+                <div className='pom_seed3 pom_seeds'></div>
+                <div className='pom_seed1 pom_seeds'></div>
+                <div className='pom_seed2 pom_seeds'></div>
+                <div className='pom_seed3 pom_seeds'></div>
 
-            </div>
+              </div>
 
-            {/* Seeds Set 2 */}
-            <div className='pom_seeds2 pom_seeds'>
-              <div className='pom_seed2 pom_seeds'></div>
-              <div className='pom_seed1 pom_seeds'></div>
-              <div className='pom_seed2 pom_seeds'></div>
-              <div className='pom_seed3 pom_seeds'></div>
-              <div className='pom_seed1 pom_seeds'></div>
-              <div className='pom_seed3 pom_seeds'></div>
+              {/* Seeds Set 2 */}
+              <div className='pom_seeds2 pom_seeds'>
+                <div className='pom_seed2 pom_seeds'></div>
+                <div className='pom_seed1 pom_seeds'></div>
+                <div className='pom_seed2 pom_seeds'></div>
+                <div className='pom_seed3 pom_seeds'></div>
+                <div className='pom_seed1 pom_seeds'></div>
+                <div className='pom_seed3 pom_seeds'></div>
 
-            </div>
+              </div>
 
 
-            {/* Seeds Set 3 CENTER*/}
-            {/* <div className='pom_seeds3 pom_seeds'>
+              {/* Seeds Set 3 CENTER*/}
+              {/* <div className='pom_seeds3 pom_seeds'>
               <div className='pom_seed1 pom_seeds'></div>
               <div className='pom_seed2 pom_seeds'></div>
               <div className='pom_seed3 pom_seeds'></div>
@@ -61,40 +63,37 @@ function Pom() {
               <div className='pom_seed1 pom_seeds'></div>
             </div> */}
 
-            {/* Seeds Set 4 */}
-            <div className='pom_seeds4 pom_seeds'>
-              <div className='pom_seed3 pom_seeds'></div>
-              <div className='pom_seed1 pom_seeds'></div>
-              <div className='pom_seed2 pom_seeds'></div>
-              <div className='pom_seed3 pom_seeds'></div>
-              <div className='pom_seed1 pom_seeds'></div> <div className='pom_seed2 pom_seeds'></div>
+              {/* Seeds Set 4 */}
+              <div className='pom_seeds4 pom_seeds'>
+                <div className='pom_seed3 pom_seeds'></div>
+                <div className='pom_seed1 pom_seeds'></div>
+                <div className='pom_seed2 pom_seeds'></div>
+                <div className='pom_seed3 pom_seeds'></div>
+                <div className='pom_seed1 pom_seeds'></div> <div className='pom_seed2 pom_seeds'></div>
+              </div>
+
+              {/* Seeds Set 5 */}
+              <div className='pom_seeds5 pom_seeds'>
+
+                <div className='pom_seed2 pom_seeds'></div>
+                <div className='pom_seed3 pom_seeds'></div>
+                <div className='pom_seed1 pom_seeds'></div>
+                <div className='pom_seed3 pom_seeds'></div>
+
+              </div>
             </div>
+            <div className='pom_inner_line'></div>
 
-            {/* Seeds Set 5 */}
-            <div className='pom_seeds5 pom_seeds'>
-
-              <div className='pom_seed2 pom_seeds'></div>
-              <div className='pom_seed3 pom_seeds'></div>
-              <div className='pom_seed1 pom_seeds'></div>
-              <div className='pom_seed3 pom_seeds'></div>
-
-            </div>
           </div>
-          <div className='pom_inner_line'></div>
 
         </div>
-
-
-
       </div>
-
 
     </div>
 
 
   )
 }
-
 
 
 export default Pom; 
