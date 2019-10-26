4d1fc8c86c3cbd79aa61932cd9e10844e9cf25a3
Anna Pawl <annampawl@gmail.com>
2019-10-25 18:12:04 -0700

    Updates

--- precache-manifest.a5c8783b710200fbda128e524224c4b3.js
+++ precache-manifest.a5c8783b710200fbda128e524224c4b3.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6fa25d9c416d0972eb33a01c5df90a52",
+    "revision": "fef4c1c14367457d7f23bcacbfd1bcdf",
     "url": "/Pure-CSS-Animations/index.html"
   },
   {
-    "revision": "8c4fcbfee2940e9c440c",
-    "url": "/Pure-CSS-Animations/static/css/main.849d3a8f.chunk.css"
+    "revision": "769ba042fd4c17ed26c5",
+    "url": "/Pure-CSS-Animations/static/css/main.44bfb3d9.chunk.css"
   },
   {
     "revision": "f9dbbe52c971418fda94",
     "url": "/Pure-CSS-Animations/static/js/2.e34e109c.chunk.js"
   },
   {
-    "revision": "8c4fcbfee2940e9c440c",
-    "url": "/Pure-CSS-Animations/static/js/main.39ad57dc.chunk.js"
+    "revision": "769ba042fd4c17ed26c5",
+    "url": "/Pure-CSS-Animations/static/js/main.e78d9cb2.chunk.js"
   },
   {
     "revision": "9d59eddc460023b0db57",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Pure-CSS-Animations/precache-manifest.771e279b0f88d93982ab59a291fd6974.js"
+  "/Pure-CSS-Animations/precache-manifest.a5c8783b710200fbda128e524224c4b3.js"
 );
 
 self.addEventListener('message', (event) => {
