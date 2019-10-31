07479b27a474e5f39041af89bf9181d24f658001
Anna Pawl <annampawl@gmail.com>
2019-10-31 14:07:49 -0700

    Updates

---
+++ precache-manifest.3343e9d5c9e6854e5735d5bb361929db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "856993ba271e597e70784c24dbb7adbc",
+    "url": "/Pure-CSS-Animations/index.html"
+  },
+  {
+    "revision": "c790df7385138a66742e",
+    "url": "/Pure-CSS-Animations/static/css/main.6af84c64.chunk.css"
+  },
+  {
+    "revision": "29114a553411672e1e38",
+    "url": "/Pure-CSS-Animations/static/js/2.c473acfa.chunk.js"
+  },
+  {
+    "revision": "c790df7385138a66742e",
+    "url": "/Pure-CSS-Animations/static/js/main.8f31639d.chunk.js"
+  },
+  {
+    "revision": "9d59eddc460023b0db57",
+    "url": "/Pure-CSS-Animations/static/js/runtime~main.a57741dc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Pure-CSS-Animations/precache-manifest.54740051b67695122642ac2b5cc380d9.js"
+  "/Pure-CSS-Animations/precache-manifest.3343e9d5c9e6854e5735d5bb361929db.js"
 );
 
 self.addEventListener('message', (event) => {
