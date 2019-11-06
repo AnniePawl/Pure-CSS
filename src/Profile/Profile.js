16686368bf0fd6d0ad2834b2da0f7380a609dfa1
Anna Pawl <annampawl@gmail.com>
2019-11-06 14:22:18 -0800

    added custom profile buttons

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,13 +1,32 @@
 import React from 'react'
+import './Profile.scss'
+import codepen from './Buttons/codepen_button1.svg'
+import github from './Buttons/github_button1.svg'
+import instagram from './Buttons/insta_button1.svg'
+import linkedin from './Buttons/linkedin_button1.svg'
+import medium from './Buttons/medium_button1.svg'
+import behance from './Buttons/behance_button1.svg'
+import dribbble from './Buttons/dribble_button1.svg'
 
 function Profile() {
+
   return (
-    <div>
-      <h1>Anna's Profile</h1>
-      <h1>Anna's Profile</h1>
-      <h1>Anna's Profile</h1>
-      <h1>Anna's Profile</h1>
+    <div className='profile_container'>
+      <h1 className='profile_header'>Anna's Profile</h1>
+
+      {/* Buttons */}
+      <div className='profile_buttons'>
+        <img src={behance} />
+        <img src={dribbble} />
+        <img src={medium} />
+        <img src={linkedin} />
+        <img src={instagram} />
+        <img src={github} />
+        <img src={codepen} />
+      </div>
+
     </div>
+
   )
 }
 
