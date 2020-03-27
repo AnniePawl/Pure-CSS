8d945ade1d90aedabf9ba3729d285114e640e11b
Anna Pawl <annampawl@gmail.com>
2020-03-27 14:45:23 -0400

    getting ready to deploy to gh pages

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,40 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { withRouter } from 'react-router-dom'
-import { HashLink } from 'react-router-hash-link'
-import './styles/Navbar.scss'
+import React from "react";
+import { Link } from "react-router-dom";
+import { withRouter } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+import "./styles/Navbar.scss";
 
 function Navbar() {
   return (
-
     <div className="nav">
-      <nav className='navbar'>
+      <nav className="navbar">
         <ul>
-
           {/* Anna Link */}
           {/* Open in New Page */}
           <li>
-            <Link className="navbar__link" to="/">My Portfolio</Link>
+            <Link className="navbar__link" to="/">
+              My Portfolio
+            </Link>
           </li>
 
           {/* This Project */}
           <li>
-            <HashLink smooth to="/#about_section"
-            >This Project</HashLink>
+            <HashLink smooth to="/#about_section">
+              This Project
+            </HashLink>
           </li>
 
           {/* More Animation Link*/}
           <li>
-            <Link className="navbar__link" to="/FruitPage">More Animations</Link>
+            <Link className="navbar__link" to="/FruitPage">
+              More Animations
+            </Link>
           </li>
-
         </ul>
       </nav>
-
-    </div >
-
-  )
+    </div>
+  );
 }
 
-export default withRouter(Navbar)
-
+export default withRouter(Navbar);
