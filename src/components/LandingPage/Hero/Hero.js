f01a807b62bd1e016e93abfa350c3265b024a872
Anna Pawl <annampawl@gmail.com>
2020-07-03 16:39:19 -0400

    updated package.json for gh pages

--- src/components/LandingPage/Hero/Hero.js
+++ src/components/LandingPage/Hero/Hero.js
@@ -1,41 +1,32 @@
-import React from 'react'
-import Buttons from '../Buttons/Buttons'
-import '../styles/LandingPage.scss'
-import { Link } from 'react-router-dom'
-
+import React from 'react';
+import Buttons from '../Buttons/Buttons';
+import '../styles/LandingPage.scss';
+import { Link } from 'react-router-dom';
 
 function Hero() {
-  return (
+	return (
+		<div className="heading">
+			{/* Landing Page Heading */}
+			<h1 className="landing_page_heading">
+				Pure CSS<br /> Animations
+			</h1>
 
-    <div className='heading'>
+			{/* Profile Buttons Component */}
+			<div className="profile_buttons">
+				<Buttons />
+			</div>
 
-      {/* Landing Page Heading */}
-      <h1 className='landing_page_heading'>
-        Pure CSS<br /> Animations
-      </h1>
+			{/* Landing Page Text */}
+			<h4 className="landing_page_text">Made With ♥ By</h4>
 
-      {/* Profile Buttons Component */}
-      <div className='profile_buttons'>
-        <Buttons />
-      </div>
-
-      {/* Landing Page Text */}
-      <h4 className='landing_page_text'>
-        Made With ♥ By
-      </h4>
-
-      {/* Link to Portfolio Site */}
-      <h4 className='landing_page_name'>
-        <Link className='name' to='/'>
-          ANNA PAWL
-        </Link>
-      </h4>
-
-    </div>
-
-  )
+			{/* Link to Portfolio Site */}
+			<h4 className="landing_page_name">
+				<Link className="name" to="/">
+					ANNA PAWL
+				</Link>
+			</h4>
+		</div>
+	);
 }
 
-export default Hero
-
-
+export default Hero;
--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,51 +1,45 @@
-import React from 'react'
-import './styles/LandingPage.scss'
+import React from 'react';
+import './styles/LandingPage.scss';
 
-import Lime from '../Fruits/Lime/Lime'
-import Hero from './Hero/Hero'
-import About from '../About/About'
-import Navbar from '../Navbar/Navbar'
+import Lime from '../Fruits/Lime/Lime';
+import Hero from './Hero/Hero';
+import About from '../About/About';
+import Navbar from '../Navbar/Navbar';
 // import Arrows from './Arrows/Arrows'
-import { withRouter } from 'react-router-dom'
-import ScrollableAnchor from 'react-scrollable-anchor'
+import { withRouter } from 'react-router-dom';
+import ScrollableAnchor from 'react-scrollable-anchor';
 
 function LandingPage() {
-  return (
-    <div className='landing_page'>
-      <Navbar />
-      <div>
-        {/* Star Animation */}
-        <div className='landing_stars'></div>
+	return (
+		<div className="landing_page">
+			<Navbar />
+			<div>
+				{/* Star Animation */}
+				<div className="landing_stars" />
 
-        {/* Hero and Lime*/}
-        <div className='landing_container'>
-          {/* Hero Text Component */}
-          <div className='hero'>
-            <Hero />
-          </div>
+				{/* Hero and Lime*/}
+				<div className="landing_container">
+					{/* Hero Text Component */}
+					<div className="hero">
+						<Hero />
+					</div>
 
-          {/* Lime Component */}
-          <div className='landing_lime'>
-            <Lime />
-          </div>
-        </div>
+					{/* Lime Component */}
+					<div className="landing_lime">
+						<Lime />
+					</div>
+				</div>
 
-        {/* Arrows Component
+				{/* Arrows Component
         <Arrows /> */}
 
-        {/* About Section */}
-        <div className='about_section'>
-          <About />
-        </div>
-
-
-      </div>
-
-    </div>
-
-  )
+				{/* About Section */}
+				<div className="about_section">
+					<About />
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default withRouter(LandingPage)
-
-
+export default withRouter(LandingPage);
