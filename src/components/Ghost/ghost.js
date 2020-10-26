64c733944ddc11d24b5e4ec4cadd452f716199a3
Anna Pawl <annampawl@gmail.com>
2020-10-26 12:51:10 -0700

    ghost

--- src/components/Ghost/ghost.js
+++ src/components/Ghost/ghost.js
@@ -6,19 +6,53 @@ function Ghost(){
     <div className='ghost_container'>
 
      <div className='ghost'>
+       {/* Ghost Ears */}
+       <div className='ghost_ears'>
+         <div className='ear1'></div>
+         <div className='ear2'></div>
+       </div>
        <div className='ghost_head'>
-         {/* Ghost Eyes */}
-         <div className='ghost_eyes'>
-           <div className='ghost_eye'></div>
-           <div className='ghost_eye'></div>
+         <div className='eyes_cheeks'>
+          {/* Ghost Eyes */}
+          <div className='ghost_eyes'>
+            <div className='outer_eye1'>
+            <div className='ghost_eye1'>
+              <div className='pupil1'></div>
+              <div className='pupil2'></div>
+            </div>
+            </div>
+            <div className='outer_eye2'>
+              <div className='ghost_eye2'>
+                <div className='pupil1'></div>
+                <div className='pupil2'></div>
+              </div>
+            </div>
+          </div>
+        
          </div>
+         
   
        </div>
-
+      {/* Ghost Body */}
        <div className='ghost_body'>
-          {/* Ghost Mouth */}
-          <div className='ghost_mouth'></div>
+            {/* Ghost Cheeks */}
+            <div className ='ghost_cheeks'>
+              <div className='cheek1'></div>
+              <div className='cheek2'></div>
+            </div>
+           {/* Ghost Mouth */}
+           <div className='ghost_mouth'></div> 
        </div>
+
+       {/* Ghost Bottom */}
+       <div className="ghost_bottom">
+         <div className='circle'></div>
+         <div className='circle'></div>
+         <div className='circle'></div>
+         <div className='circle'></div>
+         <div className='circle'></div>
+       </div>
+
      </div>
 
 
