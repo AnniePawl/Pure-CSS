0c823e81e90952fd50b956a4772e5228573dcdc2
Anna Pawl <annampawl@gmail.com>
2020-10-26 15:11:09 -0700

    cat ghost

--- src/components/Ghost/ghost.js
+++ src/components/Ghost/ghost.js
@@ -3,58 +3,77 @@ import '../Ghost/ghost.scss'
 
 function Ghost(){
   return(
+    // Ghost Container
     <div className='ghost_container'>
-
-     <div className='ghost'>
-       {/* Ghost Ears */}
-       <div className='ghost_ears'>
-         <div className='ear1'></div>
-         <div className='ear2'></div>
-       </div>
-       <div className='ghost_head'>
-         <div className='eyes_cheeks'>
-          {/* Ghost Eyes */}
-          <div className='ghost_eyes'>
-            <div className='outer_eye1'>
-            <div className='ghost_eye1'>
-              <div className='pupil1'></div>
-              <div className='pupil2'></div>
-            </div>
-            </div>
-            <div className='outer_eye2'>
-              <div className='ghost_eye2'>
-                <div className='pupil1'></div>
-                <div className='pupil2'></div>
-              </div>
-            </div>
+      {/* Ghost */}
+        <div className='ghost'>
+          {/* Ghost Ears */}
+          <div className='ghost_ears'>
+            <div className='ear1'></div>
+            <div className='ear2'></div>
           </div>
-        
-         </div>
-         
-  
-       </div>
-      {/* Ghost Body */}
-       <div className='ghost_body'>
-            {/* Ghost Cheeks */}
-            <div className ='ghost_cheeks'>
-              <div className='cheek1'></div>
-              <div className='cheek2'></div>
+          {/* Ghost Head */}
+          <div className='ghost_head'>
+              {/* Ghost Eyes */}
+              <div className='ghost_eyes'>
+                <div className='outer_eye1'>
+                  <div className='ghost_eye1'>
+                    <div className='pupil1'></div>
+                    <div className='pupil2'></div>
+                  </div>
+                </div>
+                <div className='outer_eye2'>
+                  <div className='ghost_eye2'>
+                    <div className='pupil1'></div>
+                    <div className='pupil2'></div>
+                  </div>
+                </div>
+              </div>
+          </div>
+
+          {/* Ghost Body */}
+          <div className='ghost_body'>
+            {/* Nose and Whiskers */}
+            <div className='nose_whiskers'>
+              {/* Whisker set 1 */}
+              <div className='whiskers1'>
+                <div className= 'whisker whisker1'></div>
+                <div className= 'whisker whisker2'></div>
+                <div className= 'whisker whisker3'></div>
+              </div>
+              {/* Nose */}
+              <div className='ghost_nose'></div>
+              {/* Whisker set 2 */}
+              <div className='whiskers2'>
+                <div className= 'whisker whisker4'></div>
+                <div className= 'whisker whisker5'></div>
+                <div className= 'whisker whisker6'></div>
             </div>
-           {/* Ghost Mouth */}
-           <div className='ghost_mouth'></div> 
-       </div>
+            </div>
+                {/* Ghost Cheeks */}
+                <div className ='ghost_cheeks'>
+                  <div className='cheek1'></div>
+                  <div className='cheek2'></div>
+                </div>
+              {/* Ghost Mouth */}
+              <div className='ghost_mouth'></div> 
+          </div>
 
-       {/* Ghost Bottom */}
-       <div className="ghost_bottom">
-         <div className='circle'></div>
-         <div className='circle'></div>
-         <div className='circle'></div>
-         <div className='circle'></div>
-         <div className='circle'></div>
-       </div>
+          {/* Ghost Bottom */}
+          <div className='ghost_bottom_container'>
+          <div className="ghost_bottom">
+            <div className='circle'></div>
+            <div className='circle'></div>
+            <div className='circle'></div>
+            <div className='circle'></div>
+            <div className='circle'></div>
+          </div>
+          </div>
 
-     </div>
+        </div>
 
+        <div className='ghost_shadow'></div>
+      
 
     </div>
   )
