0a55e71f823c601693aba85dc4cf58f2cdf8104f
Anna Pawl <annampawl@gmail.com>
2022-02-19 13:13:37 -0500

    book styles

--- src/components/Bookshelf/Shelf.js
+++ src/components/Bookshelf/Shelf.js
@@ -5,14 +5,15 @@ import "./books.scss";
 
 function Shelf() {
   return (
-    <div className="wall">
-      <div className="books">
-        <div className="book book4"></div>
-        <div className="book book3"></div>
-        <div className="book book1"></div>
-        <div className="book book2"></div>
-      </div>
+    // <div className="wall">
+    <div className="books">
+      <div className="book book1"></div>
+      <div className="book book2"></div>
+      <div className="book book3"></div>
+      <div className="book book4"></div>
+      <div className="book book5"></div>
     </div>
+    // </div>
   );
 }
 
