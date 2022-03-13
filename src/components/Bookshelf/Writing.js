a12be9efd2e53c24b21a6bf026ef0696fb470b38
Anna Pawl <annampawl@gmail.com>
2022-03-12 19:27:21 -0500

    pens and pencils

--- src/components/Bookshelf/Writing.js
+++ src/components/Bookshelf/Writing.js
@@ -4,11 +4,13 @@ import "./writing.scss";
 function Writing() {
   return (
     <div>
+      <div className="cup-contents">
+        <div className="ruler"></div>
+        <div className="pen"></div>
+        <div className="pencil pencil-1"></div>
+        <div className="pencil pencil-2"></div>
+      </div>
       <div className="cup"></div>
-      <div className="ruler"></div>
-      <div className="pen"></div>
-      <div className="pencil"></div>
-      <div className="pencil"></div>
     </div>
   );
 }
