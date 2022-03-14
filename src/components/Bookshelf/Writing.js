4dca65eb6357ded161f892bf6eee704098722e60
Anna Pawl <annampawl@gmail.com>
2022-03-14 19:39:01 -0400

    pen and paper

--- src/components/Bookshelf/Writing.js
+++ src/components/Bookshelf/Writing.js
@@ -3,14 +3,19 @@ import "./writing.scss";
 
 function Writing() {
   return (
-    <div>
-      <div className="cup-contents">
-        <div className="ruler"></div>
-        <div className="pen"></div>
-        <div className="pencil pencil-1"></div>
-        <div className="pencil pencil-2"></div>
+    <div className="writing-container">
+      <div className="paper"></div>
+      {/* <div className="notebook notebook1"></div>
+      <div className="notebook notebook2"></div> */}
+      <div className="cup-container">
+        <div className="cup-contents">
+          <div className="ruler cup-contents-1"></div>
+          <div className="pen cup-contents-2"></div>
+          <div className="pencil pencil-1 cup-contents-3"></div>
+          <div className="pencil pencil-2 cup-contents-4"></div>
+        </div>
+        <div className="cup"></div>
       </div>
-      <div className="cup"></div>
     </div>
   );
 }
