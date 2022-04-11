f3683e709619e25925caaf2258ed973a8b560178
Anna Pawl <annampawl@gmail.com>
2022-04-10 21:29:33 -0400

    cake

--- src/components/Bookshelf/dessert/Dessert.js
+++ src/components/Bookshelf/dessert/Dessert.js
@@ -5,6 +5,8 @@ function Dessert() {
   return (
     <div className="dessert-container">
       <div className="dessert-stand-top"></div>
+      <div className="dessert-base"></div>
+
       <div className="dessert-stand-base"></div>
     </div>
   );
